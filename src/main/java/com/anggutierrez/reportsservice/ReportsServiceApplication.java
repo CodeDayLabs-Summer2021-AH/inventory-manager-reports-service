88d7066bc24de8b0309cfa7923695cab6a8c6372
Angel <gutierrezangel240@gmail.com>
2021-08-04 23:42:25 -0700

    Initial Dev Branch

--- src/main/java/com/anggutierrez/reportsservice/ReportsServiceApplication.java
+++ src/main/java/com/anggutierrez/reportsservice/ReportsServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReportsServiceApplication {
-
+	// Initial Branch Commit
 	public static void main(String[] args) {
 		SpringApplication.run(ReportsServiceApplication.class, args);
 	}
