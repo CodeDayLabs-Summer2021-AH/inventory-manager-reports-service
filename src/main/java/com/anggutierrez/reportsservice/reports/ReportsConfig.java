c39dc02ba9a035a84de1933325ec63ff2cd49adf
Aaron Hampson <aaron.d.hampson@gmail.com>
2021-08-20 20:54:14 -0400

    Adding missing gradle dependencies

--- src/main/java/com/anggutierrez/reportsservice/reports/ReportsConfig.java
+++ src/main/java/com/anggutierrez/reportsservice/reports/ReportsConfig.java
@@ -2,14 +2,9 @@ package com.anggutierrez.reportsservice.reports;
 
 import java.util.Date;
 import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class ReportsConfig {
@@ -22,7 +17,7 @@ public class ReportsConfig {
 
             Location location = new Location(List.of(a, b, c));
 
-            reportsRepository.save(location);
+            reportsRepository.saveAll(location.getItems());
         };
     }
 }
